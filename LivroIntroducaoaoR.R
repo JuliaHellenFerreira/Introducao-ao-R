7413867514a40509a79fa8afc8103d537027151e
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-02-13 21:12:49 -0200

    Script
    Material para consulta. Livro: Introdução ao R Utilizando a Estatística Básica - Autoras: Luciane Ferreira Alcoforado e Carolina Valani Cavalcante.

---
+++ LivroIntroducaoaoR.R
@@ -0,0 +1,80 @@
+#Introdução ao R utilizando a Estatística Básica
+
+#Cap. 3 - Operações Matemáticas
+
+## Exemplos
+
+5+4 #adição
+
+6-2 #subtração
+
+7*3 #multiplicação
+
+81/9 #divisão
+
+2^2 #potência
+
+sqrt(121) #raiz
+
+exp(0) #exponencial
+
+log(1) #log na base e
+
+log10(1) #log na base 10
+
+# Cap. 4 - Armazendando Dados
+
+##Exemplos
+
+num<-4
+
+num
+
+x<-c(1,2,3,4,5)
+
+x
+
+y<-c("A", "E", "I", "O", "U" )
+
+y
+
+z<-LETTERS[1:5]
+
+z
+
+1:20
+
+seq(20,1,-1)
+
+rep(1,10)
+
+rep(1:5,2)
+
+rep(c(1,5),3)
+
+rep(c(1,5),c(3,3))
+
+x<-rep(1,5)
+
+x
+
+y<-seq(5,1,-1)
+
+y 
+
+x + y
+
+sum(x,y)
+
+sum(x) + sum(y)
+
+x*y
+
+prod(x,y)
+
+prod(x) * prod(y)
+
+append(x,y)
+
+
+
